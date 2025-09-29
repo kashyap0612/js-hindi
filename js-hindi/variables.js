4654a742a53431bebb12e74a4a2fdba60bc04752
kashyap0612 <102044009+kashyap0612@users.noreply.github.com>
2025-09-29 09:46:34 +0000

    variables

---
+++ js-hindi/variables.js
@@ -0,0 +1,12 @@
+const account_Id = 100
+let account_email = "k@gmail.com"
+var account_password = "passu"
+account_city = "Lalganj"
+let account_state
+
+// account_Id = 2 Not Allowed
+
+console.log(account_Id);
+account_email = "hc@gmail.com"
+console.log(account_email);
+console.table[account_Id,account_email,account_password,account_city,account_state]
